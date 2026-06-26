e2dbf97164335275eb6adceebeebb597e5d1bae1
alex <thesunlight17@gmail.com>
2026-06-26 21:44:52 +0500

    create block plugin

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0a1b649a877370bf83a7');
---
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cf1483c12ccea4128407');
